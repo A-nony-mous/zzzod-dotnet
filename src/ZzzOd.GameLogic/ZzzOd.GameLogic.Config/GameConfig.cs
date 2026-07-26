841e658f81c127b561ee885740971d9a34b9f71d
A-nony-mous <namesblanks@gmail.com>
2026-07-27 07:41:55 +0800

    fix(config): 手柄按键配置迁移为 32 键全集

--- src/ZzzOd.GameLogic/ZzzOd.GameLogic.Config/GameConfig.cs
+++ src/ZzzOd.GameLogic/ZzzOd.GameLogic.Config/GameConfig.cs
@@ -10,6 +10,11 @@ namespace ZzzOd.GameLogic.Config;
 /// </summary>
 public sealed class GameConfig
 {
+	/// <summary>
+	/// 旧数字索引手柄按键 到 新描述性键名 的迁移映射表。
+	/// </summary>
+	private static readonly IReadOnlyDictionary<string, string> LegacyGamepadKeyMap = BuildLegacyGamepadKeyMap();
+
 	private string? _controlMethod;
 
 	[YamlMember(Alias = "background_mode", ApplyNamingConventions = false)]
@@ -108,101 +113,293 @@ public sealed class GameConfig
 	[YamlMember(Alias = "key_chain_cancel", ApplyNamingConventions = false)]
 	public string KeyChainCancel { get; set; } = "mouse_middle";
 
+	private string _xboxKeyInteract = "xbox_a";
+
+	private string _xboxKeyNormalAttack = "xbox_x";
+
+	private string _xboxKeyDodge = "xbox_a";
+
+	private string _xboxKeySwitchNext = "xbox_rb";
+
+	private string _xboxKeySwitchPrev = "xbox_lb";
+
+	private string _xboxKeySwitchBackup = "xbox_b";
+
+	private string _xboxKeySpecialAttack = "xbox_y";
+
+	private string _xboxKeyUltimate = "xbox_rt";
+
+	private string _xboxKeyChainLeft = "xbox_lb";
+
+	private string _xboxKeyChainRight = "xbox_rb";
+
+	private string _xboxKeyMoveW = "xbox_ls_up";
+
+	private string _xboxKeyMoveS = "xbox_ls_down";
+
+	private string _xboxKeyMoveA = "xbox_ls_left";
+
+	private string _xboxKeyMoveD = "xbox_ls_right";
+
+	private string _xboxKeyLock = "xbox_r_thumb";
+
+	private string _xboxKeyChainCancel = "xbox_a";
+
+	private string _ds4KeyInteract = "ds4_cross";
+
+	private string _ds4KeyNormalAttack = "ds4_square";
+
+	private string _ds4KeyDodge = "ds4_cross";
+
+	private string _ds4KeySwitchNext = "ds4_r1";
+
+	private string _ds4KeySwitchPrev = "ds4_l1";
+
+	private string _ds4KeySwitchBackup = "ds4_circle";
+
+	private string _ds4KeySpecialAttack = "ds4_triangle";
+
+	private string _ds4KeyUltimate = "ds4_r2";
+
+	private string _ds4KeyChainLeft = "ds4_l1";
+
+	private string _ds4KeyChainRight = "ds4_r1";
+
+	private string _ds4KeyMoveW = "ds4_ls_up";
+
+	private string _ds4KeyMoveS = "ds4_ls_down";
+
+	private string _ds4KeyMoveA = "ds4_ls_left";
+
+	private string _ds4KeyMoveD = "ds4_ls_right";
+
+	private string _ds4KeyLock = "ds4_r_thumb";
+
+	private string _ds4KeyChainCancel = "ds4_cross";
+
 	[YamlMember(Alias = "xbox_key_interact", ApplyNamingConventions = false)]
-	public string XboxKeyInteract { get; set; } = "xbox_a";
+	public string XboxKeyInteract
+	{
+		get => _xboxKeyInteract;
+		set => _xboxKeyInteract = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "xbox_key_normal_attack", ApplyNamingConventions = false)]
-	public string XboxKeyNormalAttack { get; set; } = "xbox_x";
+	public string XboxKeyNormalAttack
+	{
+		get => _xboxKeyNormalAttack;
+		set => _xboxKeyNormalAttack = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "xbox_key_dodge", ApplyNamingConventions = false)]
-	public string XboxKeyDodge { get; set; } = "xbox_a";
+	public string XboxKeyDodge
+	{
+		get => _xboxKeyDodge;
+		set => _xboxKeyDodge = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "xbox_key_switch_next", ApplyNamingConventions = false)]
-	public string XboxKeySwitchNext { get; set; } = "xbox_rb";
+	public string XboxKeySwitchNext
+	{
+		get => _xboxKeySwitchNext;
+		set => _xboxKeySwitchNext = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "xbox_key_switch_prev", ApplyNamingConventions = false)]
-	public string XboxKeySwitchPrev { get; set; } = "xbox_lb";
+	public string XboxKeySwitchPrev
+	{
+		get => _xboxKeySwitchPrev;
+		set => _xboxKeySwitchPrev = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "xbox_key_switch_backup", ApplyNamingConventions = false)]
-	public string XboxKeySwitchBackup { get; set; } = "xbox_b";
+	public string XboxKeySwitchBackup
+	{
+		get => _xboxKeySwitchBackup;
+		set => _xboxKeySwitchBackup = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "xbox_key_special_attack", ApplyNamingConventions = false)]
-	public string XboxKeySpecialAttack { get; set; } = "xbox_y";
+	public string XboxKeySpecialAttack
+	{
+		get => _xboxKeySpecialAttack;
+		set => _xboxKeySpecialAttack = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "xbox_key_ultimate", ApplyNamingConventions = false)]
-	public string XboxKeyUltimate { get; set; } = "xbox_rt";
+	public string XboxKeyUltimate
+	{
+		get => _xboxKeyUltimate;
+		set => _xboxKeyUltimate = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "xbox_key_chain_left", ApplyNamingConventions = false)]
-	public string XboxKeyChainLeft { get; set; } = "xbox_lb";
+	public string XboxKeyChainLeft
+	{
+		get => _xboxKeyChainLeft;
+		set => _xboxKeyChainLeft = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "xbox_key_chain_right", ApplyNamingConventions = false)]
-	public string XboxKeyChainRight { get; set; } = "xbox_rb";
+	public string XboxKeyChainRight
+	{
+		get => _xboxKeyChainRight;
+		set => _xboxKeyChainRight = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "xbox_key_move_w", ApplyNamingConventions = false)]
-	public string XboxKeyMoveW { get; set; } = "xbox_ls_up";
+	public string XboxKeyMoveW
+	{
+		get => _xboxKeyMoveW;
+		set => _xboxKeyMoveW = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "xbox_key_move_s", ApplyNamingConventions = false)]
-	public string XboxKeyMoveS { get; set; } = "xbox_ls_down";
+	public string XboxKeyMoveS
+	{
+		get => _xboxKeyMoveS;
+		set => _xboxKeyMoveS = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "xbox_key_move_a", ApplyNamingConventions = false)]
-	public string XboxKeyMoveA { get; set; } = "xbox_ls_left";
+	public string XboxKeyMoveA
+	{
+		get => _xboxKeyMoveA;
+		set => _xboxKeyMoveA = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "xbox_key_move_d", ApplyNamingConventions = false)]
-	public string XboxKeyMoveD { get; set; } = "xbox_ls_right";
+	public string XboxKeyMoveD
+	{
+		get => _xboxKeyMoveD;
+		set => _xboxKeyMoveD = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "xbox_key_lock", ApplyNamingConventions = false)]
-	public string XboxKeyLock { get; set; } = "xbox_r_thumb";
+	public string XboxKeyLock
+	{
+		get => _xboxKeyLock;
+		set => _xboxKeyLock = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "xbox_key_chain_cancel", ApplyNamingConventions = false)]
-	public string XboxKeyChainCancel { get; set; } = "xbox_a";
+	public string XboxKeyChainCancel
+	{
+		get => _xboxKeyChainCancel;
+		set => _xboxKeyChainCancel = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "ds4_key_interact", ApplyNamingConventions = false)]
-	public string Ds4KeyInteract { get; set; } = "ds4_cross";
+	public string Ds4KeyInteract
+	{
+		get => _ds4KeyInteract;
+		set => _ds4KeyInteract = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "ds4_key_normal_attack", ApplyNamingConventions = false)]
-	public string Ds4KeyNormalAttack { get; set; } = "ds4_square";
+	public string Ds4KeyNormalAttack
+	{
+		get => _ds4KeyNormalAttack;
+		set => _ds4KeyNormalAttack = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "ds4_key_dodge", ApplyNamingConventions = false)]
-	public string Ds4KeyDodge { get; set; } = "ds4_cross";
+	public string Ds4KeyDodge
+	{
+		get => _ds4KeyDodge;
+		set => _ds4KeyDodge = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "ds4_key_switch_next", ApplyNamingConventions = false)]
-	public string Ds4KeySwitchNext { get; set; } = "ds4_r1";
+	public string Ds4KeySwitchNext
+	{
+		get => _ds4KeySwitchNext;
+		set => _ds4KeySwitchNext = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "ds4_key_switch_prev", ApplyNamingConventions = false)]
-	public string Ds4KeySwitchPrev { get; set; } = "ds4_l1";
+	public string Ds4KeySwitchPrev
+	{
+		get => _ds4KeySwitchPrev;
+		set => _ds4KeySwitchPrev = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "ds4_key_switch_backup", ApplyNamingConventions = false)]
-	public string Ds4KeySwitchBackup { get; set; } = "ds4_circle";
+	public string Ds4KeySwitchBackup
+	{
+		get => _ds4KeySwitchBackup;
+		set => _ds4KeySwitchBackup = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "ds4_key_special_attack", ApplyNamingConventions = false)]
-	public string Ds4KeySpecialAttack { get; set; } = "ds4_triangle";
+	public string Ds4KeySpecialAttack
+	{
+		get => _ds4KeySpecialAttack;
+		set => _ds4KeySpecialAttack = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "ds4_key_ultimate", ApplyNamingConventions = false)]
-	public string Ds4KeyUltimate { get; set; } = "ds4_r2";
+	public string Ds4KeyUltimate
+	{
+		get => _ds4KeyUltimate;
+		set => _ds4KeyUltimate = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "ds4_key_chain_left", ApplyNamingConventions = false)]
-	public string Ds4KeyChainLeft { get; set; } = "ds4_l1";
+	public string Ds4KeyChainLeft
+	{
+		get => _ds4KeyChainLeft;
+		set => _ds4KeyChainLeft = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "ds4_key_chain_right", ApplyNamingConventions = false)]
-	public string Ds4KeyChainRight { get; set; } = "ds4_r1";
+	public string Ds4KeyChainRight
+	{
+		get => _ds4KeyChainRight;
+		set => _ds4KeyChainRight = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "ds4_key_move_w", ApplyNamingConventions = false)]
-	public string Ds4KeyMoveW { get; set; } = "ds4_ls_up";
+	public string Ds4KeyMoveW
+	{
+		get => _ds4KeyMoveW;
+		set => _ds4KeyMoveW = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "ds4_key_move_s", ApplyNamingConventions = false)]
-	public string Ds4KeyMoveS { get; set; } = "ds4_ls_down";
+	public string Ds4KeyMoveS
+	{
+		get => _ds4KeyMoveS;
+		set => _ds4KeyMoveS = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "ds4_key_move_a", ApplyNamingConventions = false)]
-	public string Ds4KeyMoveA { get; set; } = "ds4_ls_left";
+	public string Ds4KeyMoveA
+	{
+		get => _ds4KeyMoveA;
+		set => _ds4KeyMoveA = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "ds4_key_move_d", ApplyNamingConventions = false)]
-	public string Ds4KeyMoveD { get; set; } = "ds4_ls_right";
+	public string Ds4KeyMoveD
+	{
+		get => _ds4KeyMoveD;
+		set => _ds4KeyMoveD = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "ds4_key_lock", ApplyNamingConventions = false)]
-	public string Ds4KeyLock { get; set; } = "ds4_r_thumb";
+	public string Ds4KeyLock
+	{
+		get => _ds4KeyLock;
+		set => _ds4KeyLock = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "ds4_key_chain_cancel", ApplyNamingConventions = false)]
-	public string Ds4KeyChainCancel { get; set; } = "ds4_cross";
+	public string Ds4KeyChainCancel
+	{
+		get => _ds4KeyChainCancel;
+		set => _ds4KeyChainCancel = MigrateLegacyGamepadKey(value);
+	}
 
 	[YamlMember(Alias = "xbox_action_menu", ApplyNamingConventions = false)]
 	public List<string> XboxActionMenu { get; set; }
@@ -261,6 +458,50 @@ public sealed class GameConfig
 				_controlMethod = value;
 			}
 		}
+	}
+
+	/// <summary>
+	/// 按 "+" 拆分手柄按键组合，把旧数字索引键名逐段迁移为新的描述性键名。
+	/// </summary>
+	private static string MigrateLegacyGamepadKey(string value)
+	{
+		if (string.IsNullOrEmpty(value))
+		{
+			return value;
+		}
+		string[] segments = value.Split('+');
+		for (int i = 0; i < segments.Length; i++)
+		{
+			if (LegacyGamepadKeyMap.TryGetValue(segments[i], out string? migrated))
+			{
+				segments[i] = migrated;
+			}
+		}
+		return string.Join("+", segments);
+	}
+
+	private static IReadOnlyDictionary<string, string> BuildLegacyGamepadKeyMap()
+	{
+		string[] xboxKeys = new string[14]
+		{
+			"xbox_a", "xbox_b", "xbox_x", "xbox_y", "xbox_lt", "xbox_rt", "xbox_lb", "xbox_rb", "xbox_ls_up", "xbox_ls_down",
+			"xbox_ls_left", "xbox_ls_right", "xbox_l_thumb", "xbox_r_thumb"
+		};
+		string[] ds4Keys = new string[14]
+		{
+			"ds4_cross", "ds4_circle", "ds4_square", "ds4_triangle", "ds4_l2", "ds4_r2", "ds4_l1", "ds4_r1", "ds4_ls_up", "ds4_ls_down",
+			"ds4_ls_left", "ds4_ls_right", "ds4_l_thumb", "ds4_r_thumb"
+		};
+		Dictionary<string, string> map = new Dictionary<string, string>(StringComparer.Ordinal);
+		for (int i = 0; i < xboxKeys.Length; i++)
+		{
+			map[$"xbox_{i}"] = xboxKeys[i];
+		}
+		for (int i = 0; i < ds4Keys.Length; i++)
+		{
+			map[$"ds4_{i}"] = ds4Keys[i];
+		}
+		return map;
 	}
 
 	public GameConfig()
