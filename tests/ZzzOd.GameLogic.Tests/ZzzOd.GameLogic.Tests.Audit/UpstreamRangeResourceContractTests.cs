611f031ce35cb2dfa216c9c484af28b9fa5bd5e4
A-nony-mous <namesblanks@gmail.com>
2026-08-02 06:06:44 +0800

    test(audit): 同步 3.1 资产与配置后的最新 SHA256 契约哈希

--- tests/ZzzOd.GameLogic.Tests/ZzzOd.GameLogic.Tests.Audit/UpstreamRangeResourceContractTests.cs
+++ tests/ZzzOd.GameLogic.Tests/ZzzOd.GameLogic.Tests.Audit/UpstreamRangeResourceContractTests.cs
@@ -12,10 +12,10 @@ public sealed class UpstreamRangeResourceContractTests
 	[
 		new("config/auto_battle_state_handler/速切模板-柏妮思.sample.yml", "053F7234A191A1C0CFBE5C8EBC8255B94C0E697BB3B07428BE54B25CC5C76F10"),
 		new("config/auto_battle_state_handler/速切模板-叶瞬光.sample.yml", "22C3A7B15525D4E9039773720AADE654DBD50140AEF6D58C05258653865DB63C"),
-		new("config/auto_battle/击破站场-强攻速切.merged.yml", "E521B0BDCA872F38E04DC9B81E5E81781B8EDC7D2F3C7057CA183BCEADA3B940"),
-		new("config/auto_battle/强攻站场-击破支援速切.merged.yml", "0FB051FB6FB8132D3992D343AEE58B0DFC7D9906D3BD039D28FAB2FCC8F6C44D"),
-		new("config/auto_battle/全配队通用.merged.yml", "2AC30EF3C43563549A11D0FA6C8281F020DE258B973C371EDDA5FC66353F0D37"),
-		new("config/auto_battle/自动守护.merged.yml", "FE6B44296A94D4741A1FC921AE30B1776A1C181ED558E927FF1011E48332F6C4"),
+		new("config/auto_battle/击破站场-强攻速切.merged.yml", "1D1E4399467F0CA639F441C4AF55749993A0BE823F316C07BB252811E1A496E2"),
+		new("config/auto_battle/强攻站场-击破支援速切.merged.yml", "3DB9199F68DDA058B68058CA89FD3A4C8DD1AF70B851225D42B2A18F086D37C8"),
+		new("config/auto_battle/全配队通用.merged.yml", "F39A033E010734BECAED5E5C2B50B9DC41295FAA800B063B5E2C40C024BD1138"),
+		new("config/auto_battle/自动守护.merged.yml", "F102CFBB20D05D1179C1A6EC5A53CED87D6801E6450BA84B129233937A15785F"),
 		new("config/auto_battle_operation/柏妮思-短喷.sample.yml", "7FC68398005F60C1FDA7D6F6762CE0D48E282202ED9316B2EFEA1A3955630B93"),
 	];
 
@@ -29,7 +29,7 @@ public sealed class UpstreamRangeResourceContractTests
 
 	private static readonly ResourceHash[] ScreenAndTemplateResources =
 	[
-		new("assets/game_data/screen_info/_od_merged.yml", "B9292F20584A71A6DED2C3AB85A4DBF75D0DFCC226152643883BA84E6FF5991B"),
+		new("assets/game_data/screen_info/_od_merged.yml", "47913F94233C4C9A92EAAD6256FCEB6B0EFD26B8E7186212A9331E57D6A3505B"),
 		new("assets/template/hollow/avatar_norma/raw.png", "18038B78C0F3E94638460A1E1D49EBD760741458400B497589FD30D4830145F6"),
 		new("assets/template/hollow/avatar_norma/mask.png", "5F562680464076662A086EF6E4DD356A3A5818359464F20AF4857FDC9AE0E8F6"),
 		new("assets/template/predefined_team/avatar_norma/raw.png", "158DD6DC4541EA7D25FDD020726F4776139DDC46D21AC3816AB4BE0359B320DB"),
